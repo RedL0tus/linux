8b25c1777ee226a2fd35d1e3a1d209aa712e5ad0
Russell King (Oracle) <rmk+kernel@armlinux.org.uk>
2025-11-14 15:29:00 +0000

    UPSTREAM: net: stmmac: setup default RX channel map in stmmac_plat_dat_alloc()
    Setup the default 1:1 RX channel map in stmmac_plat_dat_alloc() and
    remove 1:1 initialisations from platform glue drivers.
    
    Signed-off-by: Russell King (Oracle) <rmk+kernel@armlinux.org.uk>
    Link: https://patch.msgid.link/E1vJvjQ-0000000EVk6-05z7@rmk-PC.armlinux.org.uk
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    
    Signed-off-by: Mingcong Bai <jeffbai@aosc.io>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
@@ -7450,6 +7450,7 @@ static const struct xdp_metadata_ops stmmac_xdp_metadata_ops = {
 struct plat_stmmacenet_data *stmmac_plat_dat_alloc(struct device *dev)
 {
 	struct plat_stmmacenet_data *plat_dat;
+	int i;
 
 	plat_dat = devm_kzalloc(dev, sizeof(*plat_dat), GFP_KERNEL);
 	if (!plat_dat)
@@ -7471,6 +7472,10 @@ struct plat_stmmacenet_data *stmmac_plat_dat_alloc(struct device *dev)
 	/* Set the mtl defaults */
 	plat_dat->tx_queues_to_use = 1;
 	plat_dat->rx_queues_to_use = 1;
+
+	/* Setup the default RX queue channel map */
+	for (i = 0; i < ARRAY_SIZE(plat_dat->rx_queues_cfg); i++)
+		plat_dat->rx_queues_cfg[i].chan = i;
 
 	return plat_dat;
 }
